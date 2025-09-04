c727120a86e6c6152b4dc14c7987f0fcff079399
andreashatkontrolle-wq <andreas.hatkontrolle@gmail.com>
2025-09-04 18:23:01 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,8 +1,6 @@
-<div id="container"></div>
-<script>
-fetch('https://15b388af3e1e.ngrok-free.app/')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('container').innerHTML = html;
-  });
-</script>
+
+<?php
+$url = "https://15b388af3e1e.ngrok-free.app/";
+$html = file_get_contents($url); // Ruft die externe Seite ab
+echo $html;
+?>
